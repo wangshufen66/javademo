95948104c233e2c7364d5a302fdf593fb9075ea3
wangshufen <862965039@qq.com>
2022-09-05 01:55:18 +0800

    '部署maven'

--- Module01/src/com/wayne/DebugTest.java
+++ Module01/src/com/wayne/DebugTest.java
@@ -8,17 +8,18 @@ import java.util.HashMap;
  */
 public class DebugTest {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         for (int i = 0; i < 100; i++) {
-            System.out.println(i);        }
+            System.out.println(i);
+        }
 
 
         HashMap<String, String> map = new HashMap<>();
-        map.put("name","Tom");
-        map.put("age","12");
-        map.put("school","Tsinghua");
-        map.put("major","computer");
+        map.put("name", "Tom");
+        map.put("age", "12");
+        map.put("school", "Tsinghua");
+        map.put("major", "computer");
 
         String age = map.get("age");
         System.out.println("age = " + age);
